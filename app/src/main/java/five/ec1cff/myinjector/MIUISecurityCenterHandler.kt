ec47cee6305b808b09e8383bfd4617c82cead8d2
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-04-28 16:33:36 +0800

    fix miui permission interceptor adb not enabled

--- app/src/main/java/five/ec1cff/myinjector/MIUISecurityCenterHandler.kt
+++ app/src/main/java/five/ec1cff/myinjector/MIUISecurityCenterHandler.kt
@@ -1,9 +1,10 @@
 package five.ec1cff.myinjector
 
 import android.app.Activity
+import android.provider.Settings
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookAfter
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
@@ -13,8 +14,12 @@ class MIUISecurityCenterHandler : IXposedHookLoadPackage {
             EzXHelperInit.initHandleLoadPackage(lpparam)
             findMethod("com.miui.permcenter.privacymanager.SpecialPermissionInterceptActivity", findSuper = true) {
                 name == "onCreate"
-            }.hookAfter {
+            }.hookBefore {
                 val inst = it.thisObject as Activity
+                val permName = inst.intent.getStringExtra("permName")
+                if (permName == "miui_open_debug") {
+                    Settings.Global.putInt(inst.contentResolver, Settings.Global.ADB_ENABLED, 1)
+                }
                 inst.setResult(-1)
                 inst.finish()
             }
