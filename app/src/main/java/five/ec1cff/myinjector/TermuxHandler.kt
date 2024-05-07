531c341e2a6114c96372a3bc6a56b6acf995495e
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-05-07 20:01:56 +0800

    fix termux view infinite update loop

--- app/src/main/java/five/ec1cff/myinjector/TermuxHandler.kt
+++ app/src/main/java/five/ec1cff/myinjector/TermuxHandler.kt
@@ -5,6 +5,7 @@ import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.getObjectAs
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
@@ -17,6 +18,13 @@ class TermuxHandler : IXposedHookLoadPackage {
             name == "onCreate"
         }.hookAfter {
             it.thisObject.getObjectAs<View>("mTerminalView").setAutofillHints(null)
+        }
+        // fix termux view infinite update loop
+        // https://github.com/termux/termux-app/blob/2f40df91e54662190befe3b981595209944348e8/app/src/main/java/com/termux/app/terminal/TermuxActivityRootView.java#L120
+        findMethod("com.termux.app.terminal.TermuxActivityRootView") {
+            name == "onGlobalLayout"
+        }.hookBefore {
+            it.setResult(null)
         }
     }
 }
