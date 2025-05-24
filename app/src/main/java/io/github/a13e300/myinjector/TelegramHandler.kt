ae9daeecf2ffe870868ac6a4f093f75f8f8f7072
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-05-24 21:07:21 +0800

    tg: auto scroll to current avatar in profile activity

--- app/src/main/java/io/github/a13e300/myinjector/TelegramHandler.kt
+++ app/src/main/java/io/github/a13e300/myinjector/TelegramHandler.kt
@@ -115,6 +115,7 @@ class TelegramHandler : IHook() {
         hookHasAppToOpen()
         hookDefaultSearchTab()
         hookSetPosition()
+        hookAvatarPagerScrollToCurrent()
     }
 
     private fun hookLongClickMention() = runCatching {
@@ -944,6 +945,115 @@ class TelegramHandler : IHook() {
                 }
             }
         )
+    }
+
+    private fun hookAvatarPagerScrollToCurrent() = runCatching {
+        val pgvClass = findClass("org.telegram.ui.Components.ProfileGalleryView")
+        XposedBridge.hookAllMethods(pgvClass, "resetCurrentItem", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                if (!param.thisObject.javaClass.name.startsWith("org.telegram.ui.ProfileActivity")) return
+                val pa = param.thisObject.getObj("this$0")
+                val currentPhoto = pa.getObj("userInfo")?.getObj("profile_photo")
+                    ?: pa.getObj("chatInfo")?.getObj("chat_photo") ?: return
+                val id = currentPhoto.getObjAs<Long>("id")
+                val photos = param.thisObject.getObjAs<List<*>>("photos")
+                val idx = photos.indexOfFirst {
+                    it?.getObjAs<Long>("id") == id
+                }
+                if (idx == -1) return
+                var exactIdx = 0
+                // I know it's dumb, but ...
+                // https://github.com/DrKLO/Telegram/blob/289c4625035feafbfac355eb01591b726894a623/TMessagesProj/src/main/java/org/telegram/ui/Components/ProfileGalleryView.java#L1502
+                while (param.thisObject.call("getRealPosition", exactIdx) != idx) {
+                    exactIdx++
+                    // I believe no one can set over 300 photos
+                    if (exactIdx > 300) return
+                }
+                logD("current photo idx $idx real $exactIdx")
+                param.thisObject.call("setCurrentItem", exactIdx, false)
+                param.result = null
+            }
+        })
+        // fix wrong transition image
+        val inSetFgImg = ThreadLocal<Boolean>()
+        val paClass = findClass("org.telegram.ui.ProfileActivity")
+        XposedBridge.hookAllMethods(paClass, "setForegroundImage", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                if (param.args[0] == false) {
+                    inSetFgImg.set(true)
+                }
+            }
+
+            override fun afterHookedMethod(param: MethodHookParam) {
+                inSetFgImg.set(false)
+            }
+        })
+        XposedBridge.hookAllMethods(pgvClass, "getImageLocation", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                if (inSetFgImg.get() == true) {
+                    val pa = param.thisObject.getObj("this$0")
+                    val currentPhoto = pa.getObj("userInfo")?.getObj("profile_photo")
+                        ?: pa.getObj("chatInfo")?.getObj("chat_photo") ?: return
+                    val id = currentPhoto.getObjAs<Long>("id")
+                    val photos = param.thisObject.getObjAs<List<*>>("photos")
+                    val idx = photos.indexOfFirst {
+                        it?.getObjAs<Long>("id") == id
+                    }
+                    if (idx == -1) return
+                    param.args[0] = idx
+                    logD("fix fg img idx")
+                }
+            }
+        })
+        /*
+        val currentExactIdx = ThreadLocal<Int?>()
+        XposedBridge.hookAllMethods(
+            pgvClass, "setAnimatedFileMaybe", object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    if (!param.thisObject.javaClass.name.startsWith("org.telegram.ui.ProfileActivity")) return
+                    val pa = param.thisObject.getObj("this$0")
+                    val currentPhoto = pa.getObj("userInfo")?.getObj("profile_photo")
+                        ?: pa.getObj("chatInfo")?.getObj("chat_photo") ?: return
+                    val id = currentPhoto.getObjAs<Long>("id")
+                    val photos = param.thisObject.getObjAs<List<*>>("photos")
+                    val idx = photos.indexOfFirst {
+                        it?.getObjAs<Long>("id") == id
+                    }
+                    if (idx == -1) return
+                    var exactIdx = 0
+                    // I know it's dumb, but ...
+                    // https://github.com/DrKLO/Telegram/blob/289c4625035feafbfac355eb01591b726894a623/TMessagesProj/src/main/java/org/telegram/ui/Components/ProfileGalleryView.java#L1502
+                    while (param.thisObject.call("getRealPosition", exactIdx) != idx) {
+                        exactIdx++
+                        // I believe no one can set over 300 photos
+                        if (exactIdx > 300) return
+                    }
+                    logD("current photo for anim idx $idx real $exactIdx")
+                    currentExactIdx.set(exactIdx)
+                }
+
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    currentExactIdx.set(null)
+                }
+            }
+        )
+        val adapterClass = findClass("org.telegram.ui.Components.CircularViewPager\$Adapter")
+        XposedBridge.hookAllMethods(adapterClass, "getRealPosition", object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val exactIdx = currentExactIdx.get()
+                logD("getRealPosition ${param.args[0]} $exactIdx")
+                if (exactIdx == null) return
+
+                if (param.args[0] != exactIdx) {
+                    param.result = 114514
+                } else {
+                    logD("set fake position for anim")
+                    param.result = 0
+                }
+            }
+        })*/
+    }.onFailure {
+        logE("hookAvatarPagerScrollToCurrent", it)
     }
 }
 
