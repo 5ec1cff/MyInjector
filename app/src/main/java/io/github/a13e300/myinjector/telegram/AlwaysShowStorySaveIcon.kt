3d377da8234a269913801bcde00092eefad9a6e1
Irena <140869597+re-zero001@users.noreply.github.com>
2025-06-14 17:04:16 +0800

    tg: rewrite alwaysShowStorySaveIcon

--- app/src/main/java/io/github/a13e300/myinjector/telegram/AlwaysShowStorySaveIcon.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/AlwaysShowStorySaveIcon.kt
@@ -1,20 +1,19 @@
 package io.github.a13e300.myinjector.telegram
 
-import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
-import io.github.a13e300.myinjector.arch.hookAfter
+import io.github.a13e300.myinjector.arch.hookAllAfter
+import io.github.a13e300.myinjector.arch.setObj
 
 class AlwaysShowStorySaveIcon : DynHook() {
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.alwaysShowStorySaveIcon
     override fun onHook(loadPackageParam: LoadPackageParam) {
-        findClass("org.telegram.ui.Stories.PeerStoriesView").hookAfter(
+        findClass("org.telegram.ui.Stories.PeerStoriesView").hookAllAfter(
             "updatePosition",
             cond = ::isEnabled
         ) { param ->
-            val peerStoriesViewInstance = param.thisObject
-            XposedHelpers.setBooleanField(peerStoriesViewInstance, "allowShare", true)
-            XposedHelpers.setBooleanField(peerStoriesViewInstance, "allowRepost", true)
-            XposedHelpers.setBooleanField(peerStoriesViewInstance, "allowShareLink", true)
+            param.thisObject.setObj("allowShare", true)
+            param.thisObject.setObj("allowRepost", true)
+            param.thisObject.setObj("allowShareLink", true)
         }
     }
 }
