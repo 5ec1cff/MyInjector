d9d9fe1d65d636d6cce9a1f267938b2bebcb8d5a
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-06-21 21:31:01 +0800

    [skip ci] documentsui: add comments

--- app/src/main/java/io/github/a13e300/myinjector/DocumentsUIHandler.kt
+++ app/src/main/java/io/github/a13e300/myinjector/DocumentsUIHandler.kt
@@ -12,6 +12,8 @@ class DocumentsUIHandler : IHook() {
     override fun onHook() {
         val inOpenDocument = ThreadLocal<Boolean>()
 
+        // force add AppItem if actions == OPEN_DOCUMENT
+        // https://cs.android.com/android/platform/superproject/main/+/main:packages/apps/DocumentsUI/src/com/android/documentsui/picker/PickActivity.java;l=249;drc=28f1c8ab5c2439138de6935d632ed52f6c09fc8d
         val pickActivity = findClass("com.android.documentsui.picker.PickActivity")
         pickActivity.hookAll(
             "setupLayout",
@@ -37,6 +39,8 @@ class DocumentsUIHandler : IHook() {
 
         val inOpenRoot = ThreadLocal<Boolean>()
 
+        // fix startActivity for AppItem
+        // https://cs.android.com/android/platform/superproject/main/+/main:packages/apps/DocumentsUI/src/com/android/documentsui/picker/ActionHandler.java;l=377;drc=61197364367c9e404c7da6900658f1b16c42d0da
         val actionHandler = findClass("com.android.documentsui.picker.ActionHandler")
         actionHandler.hook(
             "openRoot",
