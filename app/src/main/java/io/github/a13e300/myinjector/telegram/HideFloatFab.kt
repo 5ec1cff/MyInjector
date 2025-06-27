a7801bccb7633eebb09157b7ee80811342f1ba8e
Irena <140869597+re-zero001@users.noreply.github.com>
2025-06-27 20:46:20 +0800

    tg: use WeakHashMap in HideFloatFab

--- app/src/main/java/io/github/a13e300/myinjector/telegram/HideFloatFab.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/HideFloatFab.kt
@@ -5,23 +5,28 @@ import android.widget.FrameLayout
 import io.github.a13e300.myinjector.arch.getObj
 import io.github.a13e300.myinjector.arch.hookAllAfter
 import io.github.a13e300.myinjector.arch.hookBefore
+import java.util.WeakHashMap
 
 class HideFloatFab : DynHook() {
+    private val fabViews = WeakHashMap<View, Boolean>()
+
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.hideFloatFab
     override fun onHook() {
         val dialogsActivityClass = findClass("org.telegram.ui.DialogsActivity")
-        val fabHashCodes = mutableSetOf<Int>()
         dialogsActivityClass.hookAllAfter("createView", cond = ::isEnabled) { param ->
             val fab1 = param.thisObject.getObj("floatingButtonContainer") as? FrameLayout
             val fab2 = param.thisObject.getObj("floatingButton2Container") as? FrameLayout
             listOfNotNull(fab1, fab2).forEach { fab ->
-                fabHashCodes.add(System.identityHashCode(fab))
+                fabViews[fab] = true
                 fab.visibility = View.GONE
             }
         }
-        findClass("android.view.View").hookBefore("setVisibility", Int::class.javaPrimitiveType!!) { param ->
+        findClass("android.view.View").hookBefore("setVisibility",
+            Int::class.javaPrimitiveType!!,
+            cond = ::isEnabled
+        ) { param ->
             val view = param.thisObject as? View ?: return@hookBefore
-            if (fabHashCodes.contains(System.identityHashCode(view)) && isEnabled()) {
+            if (fabViews.containsKey(view) && isEnabled()) {
                 param.args[0] = View.GONE
             }
         }
