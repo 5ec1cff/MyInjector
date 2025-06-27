cc69ab7a6921599a60bbe5ee718ce68c3a1bdc9d
Irena <140869597+re-zero001@users.noreply.github.com>
2025-06-27 20:57:26 +0800

    tg: fix HideFloatFab (#15)

--- app/src/main/java/io/github/a13e300/myinjector/telegram/HideFloatFab.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/HideFloatFab.kt
@@ -1,20 +1,34 @@
 package io.github.a13e300.myinjector.telegram
 
 import android.view.View
+import android.widget.FrameLayout
 import io.github.a13e300.myinjector.arch.getObj
 import io.github.a13e300.myinjector.arch.hookAllAfter
+import io.github.a13e300.myinjector.arch.hookBefore
+import java.util.WeakHashMap
 
 class HideFloatFab : DynHook() {
+    private val fabViews = WeakHashMap<View, Boolean>()
+
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.hideFloatFab
     override fun onHook() {
-        findClass("org.telegram.ui.DialogsActivity").hookAllAfter(
-            "createView",
+        val dialogsActivityClass = findClass("org.telegram.ui.DialogsActivity")
+        dialogsActivityClass.hookAllAfter("createView", cond = ::isEnabled) { param ->
+            val fab1 = param.thisObject.getObj("floatingButtonContainer") as? FrameLayout
+            val fab2 = param.thisObject.getObj("floatingButton2Container") as? FrameLayout
+            listOfNotNull(fab1, fab2).forEach { fab ->
+                fabViews[fab] = true
+                fab.visibility = View.GONE
+            }
+        }
+        findClass("android.view.View").hookBefore("setVisibility",
+            Int::class.javaPrimitiveType!!,
             cond = ::isEnabled
         ) { param ->
-            val smallFabContainer = param.thisObject.getObj("floatingButton2Container") as? View
-            smallFabContainer?.visibility = View.GONE
-            val largeFabContainer = param.thisObject.getObj("floatingButtonContainer") as? View
-            largeFabContainer?.visibility = View.GONE
+            val view = param.thisObject as? View ?: return@hookBefore
+            if (fabViews.containsKey(view)) {
+                param.args[0] = View.GONE
+            }
         }
     }
 }
