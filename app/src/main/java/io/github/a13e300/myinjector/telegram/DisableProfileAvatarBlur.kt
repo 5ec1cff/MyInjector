0778b941c6b7c4b630ffb396e549a30c50d6db7f
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-09-26 22:23:33 +0800

    tg: DisableProfileAvatarBlur: expand avatar to actions area

--- app/src/main/java/io/github/a13e300/myinjector/telegram/DisableProfileAvatarBlur.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/DisableProfileAvatarBlur.kt
@@ -1,11 +1,19 @@
 package io.github.a13e300.myinjector.telegram
 
+import android.graphics.Rect
+import android.graphics.drawable.GradientDrawable
 import android.view.View
+import android.view.ViewGroup
 import io.github.a13e300.myinjector.arch.DynHook
+import io.github.a13e300.myinjector.arch.call
+import io.github.a13e300.myinjector.arch.callS
+import io.github.a13e300.myinjector.arch.getObj
 import io.github.a13e300.myinjector.arch.getObjAs
 import io.github.a13e300.myinjector.arch.getObjAsN
 import io.github.a13e300.myinjector.arch.hookAllAfter
+import io.github.a13e300.myinjector.arch.hookAllCAfter
 import io.github.a13e300.myinjector.arch.hookAllNopIf
+import io.github.a13e300.myinjector.logD
 
 class DisableProfileAvatarBlur : DynHook() {
 
@@ -14,6 +22,7 @@ class DisableProfileAvatarBlur : DynHook() {
     override fun onHook() {
         findClass("org.telegram.ui.Components.ProfileGalleryBlurView")
             .hookAllNopIf("draw", ::isEnabled)
+        /*
         findClass("org.telegram.ui.ProfileActivity").hookAllAfter(
             "updateExtraViews",
             cond = ::isEnabled
@@ -27,6 +36,88 @@ class DisableProfileAvatarBlur : DynHook() {
                 val overlaysLp = overlaysView.layoutParams
                 overlaysLp.height -= actionsView.height
                 overlaysView.requestLayout()
+            }
+        }*/
+        findClass("org.telegram.ui.Components.ProfileGalleryView")
+            .hookAllCAfter(cond = ::isEnabled) { param ->
+                (param.thisObject as View).setPadding(0, 0, 0, 0)
+            }
+
+        findClass("org.telegram.ui.ProfileActivity\$OverlaysView").hookAllAfter(
+            "onSizeChanged",
+            cond = ::isEnabled
+        ) { param ->
+            val bottomOverlayGradient =
+                param.thisObject.getObjAs<GradientDrawable>("bottomOverlayGradient")
+            val bottomOverlayRect = param.thisObject.getObjAs<Rect>("bottomOverlayRect")
+            val actionsExtraHeight =
+                param.thisObject.getObj("this\$0").call("getActionsExtraHeight") as Int
+            bottomOverlayRect.top -= actionsExtraHeight
+            val newBounds = Rect(bottomOverlayGradient.bounds)
+            newBounds.top -= actionsExtraHeight
+            newBounds.bottom = bottomOverlayRect.top
+            bottomOverlayGradient.bounds = newBounds
+        }
+        findClass("org.telegram.ui.ActionBar.ActionBar")
+        val androidUtilities = findClass("org.telegram.messenger.AndroidUtilities")
+        val profileActivity = findClass("org.telegram.ui.ProfileActivity")
+
+        fun lerp(a: Float, b: Float, f: Float) = a + f * (b - a)
+
+        profileActivity.hookAllAfter("setAvatarExpandProgress", cond = ::isEnabled) { param ->
+            logD("setAvatarExpandProgress")
+            val actionsView = param.thisObject.getObjAsN<View>("actionsView")
+            if (actionsView == null) {
+                logD("no actionsView")
+                return@hookAllAfter
+            }
+            val avatarsViewPager = param.thisObject.getObjAs<View>("avatarsViewPager")
+            val value = param.thisObject.getObjAs<Float>("currentExpandAnimatorValue")
+            val avatarContainer = param.thisObject.getObjAs<View>("avatarContainer")
+            val avatarScale = param.thisObject.getObjAs<Float>("avatarScale")
+            val lp = avatarContainer.layoutParams as ViewGroup.MarginLayoutParams
+            val realSize = avatarsViewPager.height // extraHeight + newTop // + actionsView.height
+            val nh = lerp(
+                androidUtilities.callS("dpf2", 100f) as Float,
+                realSize / avatarScale,
+                value
+            ).toInt()
+            logD("updated: ${lp.height} -> $nh")
+            lp.height = nh
+            lp.width = nh
+            // make margin fixed, call fixAvatarImageInCenter to align it
+            lp.leftMargin = 0
+
+            param.thisObject.call("fixAvatarImageInCenter")
+            avatarContainer.requestLayout()
+        }
+
+        profileActivity.hookAllAfter("needLayout", cond = ::isEnabled) { param ->
+            val openAnimationInProgress =
+                param.thisObject.getObjAs<Boolean>("openAnimationInProgress")
+            val playProfileAnimation = param.thisObject.getObjAs<Int>("playProfileAnimation")
+
+            if (openAnimationInProgress && playProfileAnimation == 2) {
+                val avatarsViewPager = param.thisObject.getObjAs<View>("avatarsViewPager")
+                val value = param.thisObject.getObjAs<Float>("currentExpandAnimatorValue")
+                val avatarContainer = param.thisObject.getObjAs<View>("avatarContainer")
+                val avatarScale = param.thisObject.getObjAs<Float>("avatarScale")
+                val lp = avatarContainer.layoutParams as ViewGroup.MarginLayoutParams
+                val realSize =
+                    avatarsViewPager.height // extraHeight + newTop // + actionsView.height
+                val nh = lerp(
+                    androidUtilities.callS("dpf2", 100f) as Float,
+                    realSize / avatarScale,
+                    value
+                ).toInt()
+                logD("updated: ${lp.height} -> $nh")
+                lp.height = nh
+                lp.width = nh
+                // make margin fixed, call fixAvatarImageInCenter to align it
+                lp.leftMargin = 0
+
+                param.thisObject.call("fixAvatarImageInCenter")
+                avatarContainer.requestLayout()
             }
         }
     }
