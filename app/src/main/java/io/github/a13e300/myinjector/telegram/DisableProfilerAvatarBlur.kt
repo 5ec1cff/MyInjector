3453bc498a2e22d10e5e92438f9ce33d6555d898
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-09-26 17:55:28 +0800

    tg: DisableProfilerAvatarBlur: move shadow above actions view

--- app/src/main/java/io/github/a13e300/myinjector/telegram/DisableProfilerAvatarBlur.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/DisableProfilerAvatarBlur.kt
@@ -1,6 +1,10 @@
 package io.github.a13e300.myinjector.telegram
 
+import android.view.View
 import io.github.a13e300.myinjector.arch.DynHook
+import io.github.a13e300.myinjector.arch.getObjAs
+import io.github.a13e300.myinjector.arch.getObjAsN
+import io.github.a13e300.myinjector.arch.hookAllAfter
 import io.github.a13e300.myinjector.arch.hookAllNopIf
 
 class DisableProfilerAvatarBlur : DynHook() {
@@ -10,5 +14,20 @@ class DisableProfilerAvatarBlur : DynHook() {
     override fun onHook() {
         findClass("org.telegram.ui.Components.ProfileGalleryBlurView")
             .hookAllNopIf("draw", ::isEnabled)
+        findClass("org.telegram.ui.ProfileActivity").hookAllAfter(
+            "updateExtraViews",
+            cond = ::isEnabled
+        ) { param ->
+            val pa = param.thisObject
+            val overlaysView = pa.getObjAsN<View>("overlaysView") ?: return@hookAllAfter
+            val actionsView = pa.getObjAsN<View>("actionsView") ?: return@hookAllAfter
+            val isPulledDown = pa.getObjAs<Boolean>("isPulledDown")
+
+            if (isPulledDown) {
+                val overlaysLp = overlaysView.layoutParams
+                overlaysLp.height -= actionsView.height
+                overlaysView.requestLayout()
+            }
+        }
     }
 }
