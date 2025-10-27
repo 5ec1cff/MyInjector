7926e4de1ad2205ae034baf378f418b69860fbec
Irena <140869597+re-zero001@users.noreply.github.com>
2025-10-27 22:02:56 +0800

    tg: fix AutoUncheckSharePhoneNumber after version 12.1.1

--- app/src/main/java/io/github/a13e300/myinjector/telegram/AutoUncheckSharePhoneNumber.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/AutoUncheckSharePhoneNumber.kt
@@ -1,23 +1,27 @@
 package io.github.a13e300.myinjector.telegram
 
-import android.view.View
 import io.github.a13e300.myinjector.arch.DynHook
-import io.github.a13e300.myinjector.arch.call
 import io.github.a13e300.myinjector.arch.getObj
-import io.github.a13e300.myinjector.arch.hookAllAfter
+import io.github.a13e300.myinjector.arch.setObj
+import io.github.a13e300.myinjector.arch.hookAllBefore
 
 // 添加联系人时自动取消勾选分享手机号码（原行为是默认勾选）
 class AutoUncheckSharePhoneNumber : DynHook() {
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.autoUncheckSharePhoneNumber
 
     override fun onHook() {
-        findClass("org.telegram.ui.ContactAddActivity").hookAllAfter(
-            "createView",
+        findClass("org.telegram.ui.ContactAddActivity").hookAllBefore(
+            "fillItems",
             cond = ::isEnabled
         ) { param ->
-            val checkBox = param.thisObject.getObj("checkBoxCell") as? View ?: return@hookAllAfter
-            if (checkBox.call("isChecked") == true) {
-                checkBox.performClick()
+            val activity = param.thisObject
+            runCatching {
+                val checkShare = activity.getObj("checkShare") as? Boolean ?: return@hookAllBefore
+                if (!checkShare) return@hookAllBefore
+                val firstSet = activity.getObj("firstSet") as? Boolean ?: return@hookAllBefore
+                if (firstSet) {
+                    activity.setObj("checkShare", false)
+                }
             }
         }
     }
