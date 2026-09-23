e8d0d38fa80eb65d1dc776009efde93107cdfe20
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-09-23 17:10:52 +0800

    tg: support obfuscated version #16: OpenTgUserLink

--- app/src/main/java/io/github/a13e300/myinjector/telegram/OpenTgUserLink.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/OpenTgUserLink.kt
@@ -2,16 +2,36 @@ package io.github.a13e300.myinjector.telegram
 
 import android.content.Intent
 import android.net.Uri
+import io.github.a13e300.myinjector.arch.DynHook
+import io.github.a13e300.myinjector.arch.ObfsTable
+import io.github.a13e300.myinjector.arch.ObfsTableCreator
 import io.github.a13e300.myinjector.arch.hookBefore
+import io.github.a13e300.myinjector.arch.toObfsInfo
 import io.github.a13e300.myinjector.logD
 
 class OpenTgUserLink : MyDynHook("openTgUserLink") {
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.openTgUserLink
 
+    private fun deobf(creator: ObfsTableCreator): ObfsTable {
+
+        creator.create("LaunchActivityHandleIntent") { bridge ->
+            bridge.findMethod {
+                matcher {
+                    // this can't be obfuscated
+                    declaredClass("org.telegram.ui.LaunchActivity")
+                    usingEqStrings("currentAccount", "voip", "voip_answer")
+                }
+            }.single().toObfsInfo()
+        }
+        return creator.obfsTable
+    }
+
     override fun onHook() {
+        val table = deobf(TelegramHandler.creator)
+        val LaunchActivityHandleIntent = table["LaunchActivityHandleIntent"]!!
         findClass("org.telegram.ui.LaunchActivity")
             .declaredMethods
-            .filter { it.name == "handleIntent" }
+            .filter { it.name == LaunchActivityHandleIntent.memberName }
             .maxBy { it.parameterCount }
             .hookBefore { param ->
                 val intent = param.args[0] as? Intent ?: return@hookBefore
