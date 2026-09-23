2708460f6878b579c936a14221247c039fd617f1
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-09-23 16:04:23 +0800

    tg: support obfuscated version #15: FixHasAppToOpen

--- app/src/main/java/io/github/a13e300/myinjector/telegram/FixHasAppToOpen.kt
+++ app/src/main/java/io/github/a13e300/myinjector/telegram/FixHasAppToOpen.kt
@@ -3,19 +3,37 @@ package io.github.a13e300.myinjector.telegram
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import io.github.a13e300.myinjector.arch.DynHook
+import io.github.a13e300.myinjector.arch.ObfsTable
+import io.github.a13e300.myinjector.arch.ObfsTableCreator
 import io.github.a13e300.myinjector.arch.deoptimize
 import io.github.a13e300.myinjector.arch.hook
+import io.github.a13e300.myinjector.arch.toObfsInfo
 
 // 修复重复打开链接的问题
 class FixHasAppToOpen : MyDynHook("fixHasAppToOpen") {
     override fun isFeatureEnabled(): Boolean = TelegramHandler.settings.fixHasAppToOpen
 
+    private fun deobf(creator: ObfsTableCreator): ObfsTable {
+        creator.create("BrowserOpenUrl") { bridge ->
+            bridge.findMethod {
+                matcher {
+                    usingEqStrings("autologin_token", "http", "https")
+                }
+            }.single().toObfsInfo()
+        }
+        return creator.obfsTable
+    }
+
     override fun onHook() {
+        val table = deobf(TelegramHandler.creator)
+        val BrowserOpenUrl = table["BrowserOpenUrl"]!!
         val inBrowserOpenUrl = ThreadLocal<Boolean>()
         val started = ThreadLocal<Boolean>()
-        val browserClass = findClass("org.telegram.messenger.browser.Browser")
+        val browserClass = findClass(BrowserOpenUrl.className)
         val targetNethod =
-            browserClass.declaredMethods.filter { it.name == "openUrl" }.maxBy { it.parameterCount }
+            browserClass.declaredMethods.filter { it.name == BrowserOpenUrl.memberName }
+                .maxBy { it.parameterCount }
         targetNethod.hook(
             cond = ::isEnabled,
             before = {
